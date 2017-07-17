8c87ca9764184ee53da0e367de0b5713f3cd67a2
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-07-16 23:21:02 -0400

    Added click event to Add animal button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+$(document).ready( function() {
+
 var topics = ["dog", "cat", "rabbit", "hamster", 
 "skunk", "goldfish", "bird", "ferret", "turtle", 
 "sugar glider", "chinchilla", "hedgehog", "hermit crab", 
@@ -5,35 +7,69 @@ var topics = ["dog", "cat", "rabbit", "hamster",
 "serval", "salamander", "frog"];
 
 function renderButtons() {
-	//$("#animalbuttons").empty();
+	$(".animalbuttons").empty();
 
 	for (var i = 0; i < topics.length; i++) {
 		var a = $("<button>");
 		a.addClass("animal");
 		a.attr("data-name", topics[i]);
 		a.text(topics[i]);
-		$("#animalbuttons").append(a);
+		$(".animalbuttons").append(a);
 	}
+
+	
 }
+renderButtons();
 
 function getGIF() {
 	var animal = $(this).attr("data-name");
-	var queryURL = "http://api.giphy.com/?q" + animal + "&apikey=88ead5174f004521b5b5f37f78c4538a&limit=10";
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=88ead5174f004521b5b5f37f78c4538a&limit=10";
 
 	$.ajax({
 		url: queryURL,
 		method: "GET"
 	}).done(function(response) {
-		console.log(response);
-		var resultsDiv = $("<div class='results'>");
-		var rating = response.rating;
-		var r =  $("<p>").text("Rating:" + rating);
-		resultsDiv.append(r);
 
-		var gifURLStill = response.fixed_height_still;
-		var gifStill = $("<img>").attr("src", gifURLStill);
-		resultsDiv.append(gifStill);
+	$("#animals").empty();
+		
+	var results = response.data;
+	
+		for (var i = 0; i < results.length; i++) {
+			var resultsDiv = $("<div class='results'>");
+			var rating = results[i].rating;
+			var r =  $("<p>").text("Rating:" + rating);
+			resultsDiv.append(r);
+
+			var gifURLStill = results[i].images.fixed_height_still.url;
+			var gifURLAnimate = results[i].images.fixed_height.url;
+			
+			var gifStill = $("<img>").attr("src", gifURLStill);
+			gifStill.addClass("gif");
+			gifStill.attr("data-state", "still");
+			
+			resultsDiv.append(gifStill);
+			
+
+			$("#animals").append(resultsDiv);
+		}
 	});
-}
+	
+};
 
-renderButtons();
+$("#add-animal").on("click", function(event) {
+	event.preventDefault();
+
+	var newAnimal = $("#animal-input").val().trim().toLowerCase();
+	
+	topics.push(newAnimal);
+
+	renderButtons();
+});
+
+$(document).on("click", ".animal", getGIF);
+
+
+
+});
+
+
